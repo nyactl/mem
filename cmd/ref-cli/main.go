e6ccf80005b64e269732644d8bd429bb0dbb84a1
Kenji Kokubo <kenji.kokubo@rewe-group.com>
2026-05-13 22:08:13 +0200

    scaffold ref-cli: atomic tagged notes with fzf, pluggable tag/attachment backends

---
+++ cmd/ref-cli/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	if err := root.ExecuteContext(ctx); err != nil {
+		os.Exit(1)
+	}
+}
